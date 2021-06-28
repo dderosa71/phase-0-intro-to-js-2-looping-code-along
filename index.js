a2a48969453107b42cd3d2c8e95c568eef6fd253
Dominic DeRosa <dderosa71@gmail.com>
2021-06-28 17:42:45 -0500

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age ++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+function writeCards(arr,event){
+    let new_arr = []
+    for (let counter = 0 ; counter < arr.length; counter++){
+    new_arr.push(`Thank you, ${arr[counter]}, for the wonderful surprise gift!`)
+    }
+    return new_arr
+}
+
+function countDown(x){
+    while (x >= 0){
+        console.log(x--)
+}
+}
